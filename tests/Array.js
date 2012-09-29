835df3787596cc8bcdda6c14b0a72bf02ecaee84
Michael Camden <camden.michael@gmail.com>
2012-09-29 15:09:55 -0700

    Tests for array and hash are passing on both client and server

--- tests/Array.js
+++ tests/Array.js
@@ -1,10 +1,24 @@
 var assert = this['assert'] ? this['assert'] : require('assert');
+var $A = this['$A'] ? this['$A'] : require('../lib/Array.js').$A;
 
 describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
+	describe('$A', function () {
+		it('should be defined as a function', function () {
+			assert.equal(typeof $A, 'function')
+		})
+		it('should return an array when executed', function () {
+			assert(Object.isArray($A()))	
+		})
+		it('should contain 5 nodes when passed "Hello"', function () {
+			assert($A().length = 4)
+		})
+	})
+	describe('uniq', function () {
+		it('should return -> [1, 3, 2]', function () {
+			assert($A([1, 3, 2, 1]).uniq().toString() == '1,3,2')
+		})
+		it('should return -> ["A", "a"]', function () {
+			assert($A(["A","a"]).uniq().toString() == 'A,a')
+		})
+	})
 })
